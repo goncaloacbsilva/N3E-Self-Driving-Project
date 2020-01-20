d9361bbdcf3a57a48b8729ef5b130404fd824937
goncalo-MS-7C37 <weed.survive@gmail.com>
2020-01-20 22:34:25 +0000

    Added perceval based github repo checker

--- check_updates.py
+++ check_updates.py
@@ -1,7 +1,8 @@
 #! /usr/bin/env python3
 
-from perceval.backends.core.github import GitHub
-
+import os
+import json
+import time
 #Repository path
 
 repo_path = "RAM53C/Self-driving"
@@ -12,13 +13,25 @@ token = "6285432a5f20deb2b474169320e8af304efbe4b7"
 # Owner and repository names
 (owner, repo) = repo_path.split('/')
 
-# create a Git object, pointing to repo_url, using repo_dir for cloning
-repo = GitHub(owner=owner, repository=repo, api_token=token)
-# fetch all issues/pull requests as an iterator, and iterate it printing
-# their number, and whether they are issues or pull requests
-for item in repo.fetch():
-    if 'pull_request' in item['data']:
-        kind = 'Pull request'
-    else:
-        kind = 'Issue'
-    print(item['data']['number'], ':', kind)
+refresh_rate = 120
+
+def checker():
+    last_push = 0
+    while True:
+        request = os.popen('perceval github --category repository '+owner+' '+repo+' --sleep-for-rate -t '+token)
+        response = request.read()
+        response = json.loads(response)
+        push_time = response["data"]["pushed_at"]
+        push_time = push_time.replace("T", "").replace("-", "").replace(":", "").replace("Z", "")
+        if last_push != push_time:
+            print("Detected code push")
+            print("Deploying code...")
+            last_push = push_time
+            print("Finished Updating")
+            print("Push Time: "+str(push_time))
+            print("Refreshing in " + str(refresh_rate)+" seconds")
+        else:
+            print("No update detected, refreshing in " + str(refresh_rate)+" seconds")
+        time.sleep(refresh_rate)
+        
+checker()
