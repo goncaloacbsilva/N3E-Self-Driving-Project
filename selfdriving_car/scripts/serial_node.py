cce7a8e2c67eaea6d3879733f267610207846fcc
Gonçalo Corrêa Silva <weed.survive@gmail.com>
2020-02-09 18:30:40 +0000

    Updated Serial Node for working with car control

--- selfdriving_car/scripts/serial_node.py
+++ selfdriving_car/scripts/serial_node.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python
 import rospy
 import serial_rsc.serial_lib as s_libs
+import serial_rsc.CarControl as CarControl
 from std_msgs.msg import String
+
+global car
 
 def callback(data):
     rospy.loginfo("[SERIAL_NODE]: Recieved String: " + str(data.data))
@@ -10,9 +13,14 @@ def callback(data):
     speed = s_libs.convert_speed(speed)
     angle = s_libs.convert_slope(angle)
     rospy.loginfo("[SERIAL_NODE]: Velocity: " + str(speed) + " PWM / Angle: " + str(angle))
+    car.set_speed(speed)
+    car.set_steering(angle)
+    car.direction(0)
+    car.update()
 
 def module_init():
     rospy.loginfo("Initializing Serial Node...")
+    car = CarControl.Car();
     rospy.init_node('serial_node', anonymous=True)
     rospy.Subscriber("Control_out", String, callback)
     rospy.spin()
