086942dd7863fd0835e5558a1a0d8e18eecf31ab
Diogo-Abreu <diogofariaabreu@protonmail.com>
2020-02-12 02:29:39 +0000

    OpenCV Lane Warp

--- selfdriving_car/scripts/lane_object_tracking_camera.py
+++ selfdriving_car/scripts/lane_object_tracking_camera.py
@@ -1,56 +1,73 @@
 #!/usr/bin/env python
-# Software License Agreement (BSD License)
-#
-# Copyright (c) 2008, Willow Garage, Inc.
-# All rights reserved.
-#
-# Redistribution and use in source and binary forms, with or without
-# modification, are permitted provided that the following conditions
-# are met:
-#
-#  * Redistributions of source code must retain the above copyright
-#    notice, this list of conditions and the following disclaimer.
-#  * Redistributions in binary form must reproduce the above
-#    copyright notice, this list of conditions and the following
-#    disclaimer in the documentation and/or other materials provided
-#    with the distribution.
-#  * Neither the name of Willow Garage, Inc. nor the names of its
-#    contributors may be used to endorse or promote products derived
-#    from this software without specific prior written permission.
-#
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-# "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-# LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
-# FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
-# COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
-# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
-# BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-# LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
-# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
-# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-# POSSIBILITY OF SUCH DAMAGE.
-#
-# Revision $Id$
 
-## Simple talker demo that published std_msgs/Strings messages
-## to the 'chatter' topic
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as plt
 
-import rospy
-from std_msgs.msg import String
+def whiteColorFilter (frame):
 
-def talker():
-    pub = rospy.Publisher('chatter', String, queue_size=10)
-    rospy.init_node('talker', anonymous=True)
-    rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        hello_str = "hello world %s" % rospy.get_time()
-        rospy.loginfo(hello_str)
-        pub.publish(hello_str)
-        rate.sleep()
+    # It converts the BGR color space of image to HSV color space
+    hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+
+    # Threshold of white in HSV space
+    lower_white = np.array([0, 0, 175])
+    upper_white = np.array([255, 255, 255])
+
+    mask_white = cv.inRange(hsv, lower_white, upper_white)
+
+    return mask_white
+
+def cannyFilter (frame):
+
+    frameGray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+    frameBlur = cv.GaussianBlur(frameGray, (5,5) ,0)
+    frameEdge = cv.Canny(frameBlur,100,200)
+
+    return frameEdge
+
+def regionOfInterest (frame):
+    height = frame.shape[0]
+    trapeze = np.array([
+    [(500,height),(1300,height),(1100,600),(700,600)]
+    ])
+    mask = np.zeros_like(frame)
+    cv.fillPoly(mask,trapeze,255)
+    maskedFrame = cv.bitwise_and(frame,mask)
+    return maskedFrame
+
+def warp (frame):
+
+    height = frame.shape[0]
+    lenght = frame.shape[1]
+
+#    Retangulo Final
+
+    dst = np.float32([
+    [(0,height),(lenght,height),(lenght,0),(0,0)]
+    ])
+
+#    Trapezio Inicial
+
+    src =  np.float32([
+    [(500,height),(1300,height),(1100,600),(700,600)]
+    ])
+
+    Z = cv.getPerspectiveTransform(src,dst)
+    warp = cv.warpPerspective(frame,Z,(lenght,height))
+    return warp
+
 
 if __name__ == '__main__':
-    try:
-        talker()
-    except rospy.ROSInterruptException:
-        pass
+
+    frame = cv.imread('test_image.jpg',1)
+
+    white = whiteColorFilter(frame)
+    canny = cannyFilter(frame)
+    whiteCanny = cv.bitwise_or(white,canny)
+    PolymaskWhiteCanny = regionOfInterest(whiteCanny)
+    warp = warp (PolymaskWhiteCanny)
+
+    cv.imshow('yet',warp)
+    cv.waitKey(0)
+
+    cv.destroyAllWindows()
