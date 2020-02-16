9af7bd3744c09f22dacc9bcdb3409686cdaa0167
Diogo-Abreu <diogofariaabreu@protonmail.com>
2020-02-16 22:26:50 +0000

    Improved performance of lane detection

--- selfdriving_car/scripts/lane_object_tracking_camera.py
+++ selfdriving_car/scripts/lane_object_tracking_camera.py
@@ -5,16 +5,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pyzed.sl as sl
 import sys
+import time
 
 
 def whiteColorFilter(frame):
 
     # Convert the BGR color space of image to HSV color space
-
     hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-
     # Threshold of white in HSV space
-
     lower_white = np.array([0, 0, 175])
     upper_white = np.array([255, 255, 255])
     mask_white = cv.inRange(hsv, lower_white, upper_white)
@@ -32,6 +30,7 @@ def cannyFilter(frame):
 
 
 def regionOfInterest(frame, x1_l, x1_r, x2_l, x2_r):
+
     height = frame.shape[0]
     lenght = frame.shape[1]
 
@@ -47,24 +46,18 @@ def regionOfInterest(frame, x1_l, x1_r, x2_l, x2_r):
 def birdView(frame, x1_l, x1_r, x2_l, x2_r):
 
     # Setup inicial variables
-
     global trackBar3Pos
     global trackBar4Pos
     global trackBar5Pos
 
     height = frame.shape[0]
     lenght = frame.shape[1]
-
     # Inicial points for bird view transform
-
     src = np.float32([[(x2_l * lenght + (trackBar5Pos - lenght / 2), trackBar3Pos), (x1_l * lenght + (trackBar5Pos - lenght / 2), trackBar4Pos),
                        (x1_r * lenght + (trackBar5Pos - lenght / 2), trackBar4Pos), (x2_r * lenght + (trackBar5Pos - lenght / 2), trackBar3Pos)]])
-
     # Final points for bird view transform
-
     dst = np.float32([[(0 * lenght, 1 * height), (0 * lenght, 0 * height),
                        (1 * lenght, 0 * height), (1 * lenght, 1 * height)]])
-
     birdTransford = cv.getPerspectiveTransform(src, dst)
     return cv.warpPerspective(frame, birdTransford, (lenght, height))
 
@@ -101,9 +94,7 @@ def percentageConverter(frame):
 
     global trackBar1Pos
     global trackBar2Pos
-
     # Converts number from 0 - 100 to pixel coordenates
-
     length = frame.shape[1]
     center = length / 2
     x1_offset = center * trackBar1Pos / 100.0
@@ -126,7 +117,6 @@ def percentageConverter(frame):
 def getHist(frame):
 
     # Makes Histogram of the botton half of the frame
-
     hist = np.sum(frame[frame.shape[0] // 2:, :], axis=0)
     return hist
 
@@ -134,7 +124,6 @@ def getHist(frame):
 def findHistPeaks(hist):
 
     # Find the two peak is the histogram
-
     centerPoint = (hist.shape[0] // 2)
     leftPeak = np.argmax(hist[:centerPoint])
     rightPeak = np.argmax(hist[centerPoint:]) + centerPoint
@@ -145,18 +134,19 @@ def findHistPeaks(hist):
 def slidingBox(frame, leftPeak, rightPeak, numberOfBoxes, boxMargin):
 
     # Declaring necessary arrays
-
     leftBoxPixelsX = []
     leftBoxPixelsY = []
-    tempLeftBoxPixelsX = []
-    tempLeftBoxPixelsY = []
-
     rightBoxPixelsX = []
     rightBoxPixelsY = []
-    tempRightBoxPixelsX = []
-    tempRightBoxPixelsY = []
+
+    leftLaneNonZeroPixels = []
+    rightLaneNonZeroPixels = []
 
     # Getting some inicial values
+
+    nonzero = frame.nonzero()
+    nonzeroy = np.array(nonzero[0])
+    nonzerox = np.array(nonzero[1])
 
     leftBoxCenter = leftPeak
     rightBoxCenter = rightPeak
@@ -168,11 +158,8 @@ def slidingBox(frame, leftPeak, rightPeak, numberOfBoxes, boxMargin):
     color_warp = np.dstack((warp_zero, warp_zero, warp_zero))
 
     # Loops that goes thourgh all the boxes
-
     for window in range(numberOfBoxes):
-
         # Determine X and Y coordenates of next couple of boxes
-
         lowY = frame.shape[0] - (window * boxHeight)
         upperY = lowY - boxHeight
 
@@ -181,71 +168,39 @@ def slidingBox(frame, leftPeak, rightPeak, numberOfBoxes, boxMargin):
 
         rightBoxLeftX = rightBoxCenter - boxMargin
         rightBoxRightX = rightBoxCenter + boxMargin
-
-        # Make sure the values down go beyond the window
-
-        if upperY < 0:
-            upperY = 0
-        if lowY > frame.shape[0]:
-            lowY = frame.shape[0]
-
-        if leftBoxLeftX < 0:
-            leftBoxLeftX = 0
-        if leftBoxRightX > frame.shape[1]:
-            leftBoxRightX = frame.shape[1]
-
-        if rightBoxLeftX < 0:
-            rightBoxLeftX = 0
-        if rightBoxRightX > frame.shape[1]:
-            rightBoxRightX = frame.shape[1]
-
-        # Show boxes in the screen
-
+        # Show boxes in screen
         cv.rectangle(color_warp, (leftBoxLeftX, lowY), (leftBoxRightX, upperY),
                      (100, 255, 255), 1)
         cv.rectangle(color_warp, (rightBoxLeftX, lowY), (rightBoxRightX, upperY),
                      (100, 255, 255), 1)
+        # get pixels that are not zero
+        leftBoxNonZeroPixels = ((nonzeroy >= upperY) & (nonzeroy < lowY) & (
+            nonzerox >= leftBoxLeftX) & (nonzerox < leftBoxRightX)).nonzero()[0]
+        rightBoxNonZeroPixels = ((nonzeroy >= upperY) & (nonzeroy < lowY) & (
+            nonzerox >= rightBoxLeftX) & (nonzerox < rightBoxRightX)).nonzero()[0]
+        # Append these indices to the lists
+        leftLaneNonZeroPixels.append(leftBoxNonZeroPixels)
+        rightLaneNonZeroPixels.append(rightBoxNonZeroPixels)
+        # Update center of next boxes, but there needs to be at least 10 pixels in the box
+        if len(leftBoxNonZeroPixels) > 10:
+            leftBoxCenter = np.int(np.mean(nonzerox[leftBoxNonZeroPixels]))
+        if len(rightBoxNonZeroPixels) > 10:
+            rightBoxCenter = np.int(np.mean(nonzerox[rightBoxNonZeroPixels]))
 
-        # creates array with all of the X and Y coordenates of the pixels in the box
-
-        tempLeftBoxPixelsX.clear()
-        tempLeftBoxPixelsY.clear()
-        for y in range(upperY, lowY):
-            for x in range(leftBoxLeftX, leftBoxRightX):
-                if frame[y, x] != 0:
-                    tempLeftBoxPixelsX.append(x)
-                    tempLeftBoxPixelsY.append(y)
-                    leftBoxPixelsX.append(x)
-                    leftBoxPixelsY.append(y)
-
-        tempRightBoxPixelsX.clear()
-        tempRightBoxPixelsY.clear()
-        for y in range(upperY, lowY):
-            for x in range(rightBoxLeftX, rightBoxRightX):
-                if frame[y, x] != 0:
-                    tempRightBoxPixelsX.append(x)
-                    tempRightBoxPixelsY.append(y)
-                    rightBoxPixelsX.append(x)
-                    rightBoxPixelsY.append(y)
-
-        # Update center of next boxes, but there
-        # needs to be at least 10 pixels in the box
-
-        if len(tempLeftBoxPixelsX) > 10:
-            leftBoxCenter = np.int(np.mean(np.array([tempLeftBoxPixelsX])))
-
-        if len(tempRightBoxPixelsX) > 10:
-            rightBoxCenter = np.int(np.mean(np.array([tempRightBoxPixelsX])))
-
+    leftLaneNonZeroPixels = np.concatenate(leftLaneNonZeroPixels)
+    rightLaneNonZeroPixels = np.concatenate(rightLaneNonZeroPixels)
+    # get X and Y pixels positions
+    leftLanePixelsX = nonzerox[leftLaneNonZeroPixels]
+    leftLanePixelsY = nonzeroy[leftLaneNonZeroPixels]
+    rightLanePixelsX = nonzerox[rightLaneNonZeroPixels]
+    rightLanePixelsY = nonzeroy[rightLaneNonZeroPixels]
     # Get all lines polynomes and determine average
-    if len(leftBoxPixelsX) > 3 or len(leftBoxPixelsY) > 3:
-        leftPoly = np.polyfit(leftBoxPixelsY, leftBoxPixelsX, 2)
+    if len(leftLanePixelsX) > 3 or len(leftLanePixelsY) > 3:
+        leftPoly = np.polyfit(leftLanePixelsY, leftLanePixelsX, 2)
         leftPoly[0] = np.mean(leftPoly[0])
         leftPoly[1] = np.mean(leftPoly[1])
         leftPoly[2] = np.mean(leftPoly[2])
-
         # Find line coordenates in order to draw Polinomial line
-
         leftPolyY = np.linspace(0, frame.shape[0] - 1, frame.shape[0])
         leftPolyX = leftPoly[0] * leftPolyY**2 + \
             leftPoly[1] * leftPolyY + leftPoly[2]
@@ -257,14 +212,12 @@ def slidingBox(frame, leftPeak, rightPeak, numberOfBoxes, boxMargin):
 
         calculateCurvature(color_warp, leftPoly, leftPeak)
 
-    if len(rightBoxPixelsX) > 3 or len(rightBoxPixelsY) > 3:
-        rightPoly = np.polyfit(rightBoxPixelsY, rightBoxPixelsX, 2)
+    if len(rightLanePixelsX) > 3 or len(rightLanePixelsY) > 3:
+        rightPoly = np.polyfit(rightLanePixelsY, rightLanePixelsX, 2)
         rightPoly[0] = np.mean(rightPoly[0])
         rightPoly[1] = np.mean(rightPoly[1])
         rightPoly[2] = np.mean(rightPoly[2])
-
         # Find line coordenates in order to draw Polinomial line
-
         rightPolyY = np.linspace(0, frame.shape[0] - 1, frame.shape[0])
         rightPolyX = rightPoly[0] * rightPolyY**2 + \
             rightPoly[1] * rightPolyY + rightPoly[2]
@@ -274,8 +227,9 @@ def slidingBox(frame, leftPeak, rightPeak, numberOfBoxes, boxMargin):
         cv.polylines(color_warp, np.int32([rightPolyPoints]), isClosed=False, color=(
             200, 255, 155), thickness=4)
 
-    # Add both images together
+        calculateCurvature(color_warp, rightPoly, rightPeak)
 
+    # Add both images together
     frame = cv.cvtColor(frame, cv.COLOR_GRAY2RGB)
     frame = cv.addWeighted(frame, 0.7, color_warp, 0.8, 0)
 
@@ -283,20 +237,14 @@ def slidingBox(frame, leftPeak, rightPeak, numberOfBoxes, boxMargin):
 
 
 def calculateCurvature(frame, Polinome, Peak):
-
     # Determine distance that corresponds to one pixel
-
     distancePerPixelX = 15.0 / frame.shape[1]
     distancePerPixelY = 3.7 / frame.shape[0]
-
     # Find first and second derivative of Polinomial equacion
-
     D1 = (2 * Polinome[0] * Peak + Polinome[1]) * \
         (distancePerPixelX / distancePerPixelY)
     D2 = (2 * Polinome[0]) * (distancePerPixelX / distancePerPixelY)
-
     # Use the circle radius and curve angle
-
     curve = ((1 + D1 * D1)**(3 / 2)) / np.absolute(D2)
     angle = 18000 / (3.14 * curve)
     print(angle)
@@ -310,17 +258,18 @@ def drawLines(frame, x1_l, x1_r, x2_l, x2_r):
     global trackBar4Pos
     global trackBar5Pos
 
+    cv.line(frame, (int(x1_l * frame.shape[1] + (trackBar5Pos - frame.shape[1] / 2)), trackBar4Pos), (int(
+        x2_l * frame.shape[1] + (trackBar5Pos - frame.shape[1] / 2)), trackBar3Pos), (0, 255, 255))
 
-    cv.line(frame, (int(x1_l * frame.shape[1] + (trackBar5Pos - frame.shape[1] / 2)), trackBar4Pos),(int(x2_l * frame.shape[1] + (trackBar5Pos - frame.shape[1] / 2)), trackBar3Pos), (0, 255, 255))
-
-    cv.line(frame, (int(x1_r * frame.shape[1] + (trackBar5Pos - frame.shape[1] / 2)), trackBar4Pos),(int(x2_r * frame.shape[1] + (trackBar5Pos - frame.shape[1] / 2)), trackBar3Pos), (0, 255, 255))
+    cv.line(frame, (int(x1_r * frame.shape[1] + (trackBar5Pos - frame.shape[1] / 2)), trackBar4Pos), (int(
+        x2_r * frame.shape[1] + (trackBar5Pos - frame.shape[1] / 2)), trackBar3Pos), (0, 255, 255))
 
     return frame
 
 
 def main():
 
-    # Magical camera setup made by the gods at ZED
+        # Magical camera setup made by the gods at ZED
 
     zed = sl.Camera()
     input_type = sl.InputType()
@@ -341,29 +290,43 @@ def main():
     image_size.height = image_size.height / 2
     image_zed = sl.Mat(image_size.width, image_size.height, sl.MAT_TYPE.U8_C4)
 
-    zed.grab(runtime)
-    zed.retrieve_image(image_zed, sl.VIEW.LEFT, sl.MEM.CPU, image_size)
-    frame = image_zed.get_data()
+    cap = cv.VideoCapture('test_video.mp4')
+    ret, frame1 = cap.read()
 
-    createTrackBar(frame)
+    createTrackBar(frame1)
     onChangeTrackBar(0)
 
     while True:
+
         zed.grab(runtime)
         zed.retrieve_image(image_zed, sl.VIEW.LEFT, sl.MEM.CPU, image_size)
         frame = image_zed.get_data()
-        x1_l, x1_r, x2_l, x2_r = percentageConverter(frame)
-        white = whiteColorFilter(frame)
-        canny = cannyFilter(frame)
+
+        ret, frame1 = cap.read()
+
+        x1_l, x1_r, x2_l, x2_r = percentageConverter(frame1)
+        white = whiteColorFilter(frame1)
+        canny = cannyFilter(frame1)
         whiteCanny = cv.bitwise_or(white, canny)
         birdFrame = birdView(whiteCanny, x1_l, x1_r, x2_l, x2_r)
         hist = getHist(birdFrame)
         leftPeak, rightPeak = findHistPeaks(hist)
         final = slidingBox(birdFrame, leftPeak, rightPeak, 15, 100)
-        frameWithLines = drawLines(frame, x1_l, x1_r, x2_l, x2_r)
-        cv.imshow('OPENCV TEST2', frameWithLines)
-        cv.imshow('OPENCV TEST1', final)
-        k = cv.waitKey(30)
+        frameWithLines = drawLines(frame1, x1_l, x1_r, x2_l, x2_r)
+
+        scale_percent = 60
+        width = int(frame1.shape[1] * scale_percent / 100)
+        height = int(frame1.shape[0] * scale_percent / 100)
+        dim = (width, height)
+
+        resised1 = cv.resize(frameWithLines, dim)
+        resised2 = cv.resize(final, dim)
+
+        vis = np.concatenate((resised1, resised2), axis=1)
+
+        cv.imshow('OPENCV TEST1', vis)
+
+        k = cv.waitKey(10)
         if k == 32:
             break
 
